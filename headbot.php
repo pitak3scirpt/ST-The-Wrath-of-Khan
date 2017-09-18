6c9172363d19e0797d35b321dd8439bfeea5e848
Jedi Council <pitak3g.sc@gmail.com>
2017-09-18 20:20:33 +0700

    Update headbot.php

--- headbot.php
+++ headbot.php
@@ -1,8 +1,18 @@
 <?php
 
-$access_token = 'juzbEAu+T7V0MLnZZevyeLYtSBGX0WGpS8Dcq7SPHFZvuWkT9FKm3NYrSk/ED8sP0SBK4u1c8pZyFSh5+geh2jDka8GI7pPh0DtIBGqeAgj5W9Gx3e/0+A/p66+EHMixoQVE1NH5FeNEpbdIzAe3NQdB04t89/1O/w1cDnyilFU=';
+//Pitak SC
+$proxy = 'velodrome.usefixie.com:80';
+$proxyauth = 'fixie:ncIwXPyhNi4cEWL';
+
+//Token Master Yoda
+$access_token = 'TuPeAEFb91uGz4kPxjnMeZ6QaAGrLK05ZQcO5P1zdTNr3bYhATznR5S9ef2Xr/7uNFmngRzqMu+xPUsXw3u53QOwzy2SP+RsQsMVxP6G7VxEff/5I7k0t+SrazH97wQpwxIblRSY7FLpoTkwY0l5hQdB04t89/1O/w1cDnyilFU=';
+// Token MacShare
+//$access_token = 'juzbEAu+T7V0MLnZZevyeLYtSBGX0WGpS8Dcq7SPHFZvuWkT9FKm3NYrSk/ED8sP0SBK4u1c8pZyFSh5+geh2jDka8GI7pPh0DtIBGqeAgj5W9Gx3e/0+A/p66+EHMixoQVE1NH5FeNEpbdIzAe3NQdB04t89/1O/w1cDnyilFU=';
 
 $baseUrl = "https://ecs.egat.co.th/public.php?service=files&t=fd20b4335410e38c017713bd6d458deb";
+
+//MacShare egat clound Service
+$ecsURL = "https://ecs.egat.co.th/index.php/apps/gallery/ajax/image.php?file=fd20b4335410e38c017713bd6d458deb%2F%2F;
 
 //https://still-inlet-83395.herokuapp.com/
 
